2be638b872b8967f0562eef006f7c6b5dc3bb5c0
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-06-30 12:01:31 +0200

    contao 3.5 compatibility

--- drivers/DC_Hybrid.php
+++ drivers/DC_Hybrid.php
@@ -10,4 +10,28 @@ class DC_Hybrid extends \DataContainer
 		$this->strTable = $strTable;
 		$this->objModule = $objModule;
 	}
+
+	/**
+	 * Return the name of the current palette
+	 *
+	 * @return string
+	 */
+	public function getPalette()
+	{
+		// TODO: Implement getPalette() method.
+	}
+
+	/**
+	 * Save the current value
+	 *
+	 * @param mixed $varValue
+	 *
+	 * @throws \Exception
+	 */
+	protected function save($varValue)
+	{
+		// TODO: Implement save() method.
+	}
+
+
 }
