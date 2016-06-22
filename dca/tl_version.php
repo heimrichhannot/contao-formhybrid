8e3ff96189cd8aa9c118b3d6a76e4e0d04129403
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-06-22 13:49:51 +0200

    fixed versions

--- dca/tl_version.php
+++ dca/tl_version.php
@@ -4,11 +4,11 @@ $arrDca = &$GLOBALS['TL_DCA']['tl_version'];
 
 $arrFields = array(
 	'memberusername' => array(
-		'sql'                     => "varchar(255) NOT NULL default ''"
+		'sql' => "varchar(255) NULL"
 	),
 	'memberid' => array(
-		'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		'sql' => "int(10) unsigned NULL"
 	)
 );
 
-$arrDca['fields'] = array_merge($arrFields,  $arrDca['fields']);
+$arrDca['fields'] = array_merge($arrFields, $arrDca['fields']);
