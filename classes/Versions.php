bfa778fee9534fdc85a8ea8251615fac382e2ef1
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-06-30 14:43:05 +0200

    fixed versions bug

--- classes/Versions.php
+++ classes/Versions.php
@@ -588,7 +588,8 @@ class Versions extends \Controller
 		$objTemplate->pagination = $objPagination->generate();
 
 		// Get the versions
-		$objVersions = $objDatabase->prepare("SELECT pid, tstamp, version, fromTable, username, userid, memberusername, memberid, formhybrid_backend_url, description, editUrl, active FROM tl_version" . (!$objUser->isAdmin ? " WHERE userid=?" : "") . " ORDER BY tstamp DESC, pid, version DESC")
+		$objVersions = $objDatabase->prepare("SELECT pid, tstamp, version, fromTable, username, userid, description, editUrl, active" .
+			(\Database::getInstance()->fieldExists('memberusername', 'tl_version') ? ', memberusername, memberid, formhybrid_backend_url' : '') . " FROM tl_version" . (!$objUser->isAdmin ? " WHERE userid=?" : "") . " ORDER BY tstamp DESC, pid, version DESC")
 								   ->limit(30, $intOffset)
 								   ->execute($objUser->id);
 
