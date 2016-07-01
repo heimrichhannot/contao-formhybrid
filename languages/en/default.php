80619c11fba6055932eda7c3106df0c25e1cf208
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-07-01 14:25:12 +0200

    translation

---
+++ languages/en/default.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Content Elements
+ */
+$GLOBALS['TL_LANG']['CTE']['formhybrid'] = 'Formhybrid';
+$GLOBALS['TL_LANG']['CTE']['formhybridStart'] = array('Form Start','Declares the start of a form');
+$GLOBALS['TL_LANG']['CTE']['formhybridElement'] = array('Form Element','Insert elements into the form');
+$GLOBALS['TL_LANG']['CTE']['formhybridStop'] = array('Form Stop','Declares the stop of a form');
+
+/**
+ * Submit Labels
+ */
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['submitLabels']['default'] = 'Submit';
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['submitLabels']['filter'] = 'Filter';
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['submitLabels']['orderWithCosts'] = 'Order';
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['submitLabels']['orderFree'] = 'Order for free';
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['submitLabels']['order'] = 'Place an order';
+
+/**
+ * Messages
+ */
+$GLOBALS['TL_LANG']['formhybrid']['messages']['success'] = 'Thanks for your submission.';
+$GLOBALS['TL_LANG']['formhybrid']['messages']['error']['invalidId'] = 'No record matches the given id.';
+
+/**
+ * Misc
+ */
+$GLOBALS['TL_LANG']['MSC']['formhybrid']['memberVersion'] = ' (Member)';
